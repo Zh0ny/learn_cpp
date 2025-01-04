686353760e0bbfef21dd56cb611517dc4178c53a
Zh0ny <caio.philipe.queiroz@gmail.com>
2025-01-03 21:04:53 -0300

    C++ Basics using learncpp Chapter 1 and section 1.5: 	-1.5 Introduction to iostream: cout, cin, and endl: 		-1.5.6 std::cin 		-1.5.7 std::cin is buffered 		- Question 1 		- Question 2

--- main.cpp
+++ main.cpp
@@ -1,71 +1,51 @@
 #include <iostream>
 
+// This function represent the use of std::cin to get input from the user, about section 1.5.6, Question 1 and 2
 int main()
 {
-    std::string text { "Hello World!" }; // direct-list-initilization
-    std::cout << text << std::endl;
+    // std::cin
+    std::cout << "Enter a number: ";
+    int x {};
 
-    [[maybe_unused]] int a;     // default-initialization (no initializer) with [[maybe_unused]] attribute c++17
+    std::cin >> x;
 
-    //std::cout << "Variable a value = " + a << std::endl; //error: local variable not initialized
+    std::cout << "You entered: " << x << '\n';
 
-    // Variable Assignment
+    std::cout << "Enter two numbers separated by a space: ";
+    int x2 {};
+    int y {};
 
-    a = 5; // copy assignment of value 5 into variable width
-    std::cout << a ; 
-    a = 7; // change value stored in variable width to 7
-    std::cout << a ;
+    std::cin >> x2 >> y;
 
-    // Variable initialization
-    int width {5};       // define variable width and initialize with initial value 5
-    std::cout << width; // prints 5
+    std::cout << "You entered " << x2 << " and " << y << '\n';
 
-    // Different forms of initialization
+    // std::cin is buffered
 
-    // Traditional initialization forms:
-    int b = 5;     // copy-initialization (initial value after equals sign)
-    std::cout << "Variable b value: " << b << std::endl;
-    int c ( 6 );   // direct-initialization (initial value in parenthesis)
-    std::cout << "Variable c value: " << c << std::endl;
+    std::cout << "Enter two numbers: ";
 
-    // Modern initialization forms (preferred):
-    int d { 7 };   // direct-list-initialization (initial value in braces)
-    std::cout << "Variable d value: " << d << std::endl;
-    int e {};      // value-initialization (empty braces)
-    std::cout << "Variable e value: " << e << std::endl;
+    int x3{};
+    std::cin >> x3;
 
-    // List-initialization disallows narrowing conversions
+    int y2{};
+    std::cin >> y2;
 
-    // An integer can only hold non-fractional values.
-    // Initializing an int with fractional value 4.5 requires the compiler to convert 4.5 to a value an int can hold.
-    // Such a conversion is a narrowing conversion, since the fractional part of the value will be lost.
+    std::cout << "You entered " << x3 << " and " << y2 << '\n';
 
-    //int w1 { 4.5 }; //compile error: list-init does not allow narrowing conversion
+    // Question 1
 
-    //int w2 = 4.5;   // compiles: w2 copy-initialized to value 4, gives error of narrowing conversion
-    //std::cout << "Variable w2 value: " + w2 << std::endl;
-    //int w3 (4.5);   // compiles: w3 direct-initialized to value 4, gives error of narrowing conversion
-    //std::cout << "Variable w3 value: " + w3 << std::endl;
-    
-    // Initializing multiple variables
+    std::cout << "Enter a number: ";
+    int x4 {};
+    std::cin >> x4;
+    std::cout << "You entered: " << x4 << '\n';
 
-    int a2 = 5, b2 = 6;          // copy-initialization
-    int c2 ( 7 ), d2 ( 8 );      // direct-initialization
-    int e2 { 9 }, f { 10 };     // direct-list-initialization
-    int i {}, j {};            // value-initialization
+    //Question 2
 
-    std::cout << a2 << b2 << c2 << d2 << e2 << f << i << j << std::endl;
-
-    // The [[maybe_unused]] attribute C++17
-
-    [[maybe_unused]] double pi { 3.14159 };  // Don't complain if pi is unused
-    [[maybe_unused]] double gravity { 9.8 }; // Don't complain if gravity is unused
-    [[maybe_unused]] double phi { 1.61803 }; // Don't complain if phi is unused
-
-    std::cout << pi << '\n';
-    std::cout << phi << '\n';
-
-    // The compiler will no longer warn about gravity not being used
+    std::cout << "Enter three numbers: ";
+    int x5 {};
+    int y3 {};
+    int z {};
+    std::cin >> x5 >> y3 >> z;
+    std::cout << "You entered " << x5 << ", " << y3 << ", and " << z << '\n';
 
     return 0;
 }
