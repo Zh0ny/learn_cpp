dea157f5a1014e4bb73993fa799cd2bc5ab0e1da
Zh0ny <caio.philipe.queiroz@gmail.com>
2025-01-04 00:49:51 -0300

    C++ Basics using learncpp Chapter 1 and section 1.6: 	1.6 Uninitialized variables and undefined behavior: 		-1.6.1 Uninitialized variables

--- main.cpp
+++ main.cpp
@@ -1,51 +1,18 @@
 #include <iostream>
 
-// This function represent the use of std::cin to get input from the user, about section 1.5.6, Question 1 and 2
+void doNothing(int&) // Don't worry about what & is for now, we're just using it to trick the compiler into thinking variable x is used
+{
+}
+
 int main()
 {
-    // std::cin
-    std::cout << "Enter a number: ";
-    int x {};
+    // define an integer variable named x
+    int x; // this variable is uninitialized
 
-    std::cin >> x;
+    doNothing(x); // make the compiler think we're assigning a value to this variable
 
-    std::cout << "You entered: " << x << '\n';
-
-    std::cout << "Enter two numbers separated by a space: ";
-    int x2 {};
-    int y {};
-
-    std::cin >> x2 >> y;
-
-    std::cout << "You entered " << x2 << " and " << y << '\n';
-
-    // std::cin is buffered
-
-    std::cout << "Enter two numbers: ";
-
-    int x3{};
-    std::cin >> x3;
-
-    int y2{};
-    std::cin >> y2;
-
-    std::cout << "You entered " << x3 << " and " << y2 << '\n';
-
-    // Question 1
-
-    std::cout << "Enter a number: ";
-    int x4 {};
-    std::cin >> x4;
-    std::cout << "You entered: " << x4 << '\n';
-
-    //Question 2
-
-    std::cout << "Enter three numbers: ";
-    int x5 {};
-    int y3 {};
-    int z {};
-    std::cin >> x5 >> y3 >> z;
-    std::cout << "You entered " << x5 << ", " << y3 << ", and " << z << '\n';
+    // print the value of x to the screen (who knows what we'll get, because x is uninitialized)
+    std::cout << x << '\n';
 
     return 0;
 }
