9398f298246a87291a55c6274c613493fe993058
Zh0ny <caio.philipe.queiroz@gmail.com>
2025-01-05 02:37:35 -0300

    C++ Basics using learncpp Chapter 1 and section 1.8: 	-1.8 Whitespace and basic formatting: 		-1.8.4 Basic formatting

--- main.cpp
+++ main.cpp
@@ -30,5 +30,44 @@ int main()
 
     std::cout << "This one is short\n";
 
+    std::cout << 3 + 4
+    + 5 + 6
+    * 7 * 8;
+    /* Harder to read
+        cost = 57;
+        pricePerItem = 24;
+        value = 5;
+        numberOfItems = 17;
+    */
+    //easy to read
+    int cost          = 57;
+    int pricePerItem  = 24;
+    int value         = 5;
+    int numberOfItems = 17;
+
+    std::cout << "Hello world!\n"; // cout lives in the iostream library
+    std::cout << "It is very nice to meet you!\n"; // these comments make the code hard to read
+    std::cout << "Yeah!\n"; // especially when lines are different lengths
+
+    std::cout << "Hello world!\n";                  // cout lives in the iostream library
+    std::cout << "It is very nice to meet you!\n";  // these comments are easier to read
+    std::cout << "Yeah!\n";                         // especially when all lined up
+
+    // cout lives in the iostream library
+    std::cout << "Hello world!\n";
+    // these comments make the code hard to read
+    std::cout << "It is very nice to meet you!\n";
+    // especially when all bunched together
+    std::cout << "Yeah!\n";
+
+    // cout lives in the iostream library
+    std::cout << "Hello world!\n";
+
+    // these comments are easier to read
+    std::cout << "It is very nice to meet you!\n";
+
+    // when separated by whitespace
+    std::cout << "Yeah!\n";
+
     return 0;
 }
