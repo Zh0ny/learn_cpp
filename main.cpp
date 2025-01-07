0a3a5c1a0efd0434f7ed6870222bfcf7fcee665d
Zh0ny <caio.philipe.queiroz@gmail.com>
2025-01-06 23:39:29 -0300

    C++ Basics using learncpp Chapter 1 and section 1.11: 	-1.11 Developing your first program: 		-1.11.1 Multiply by 2; 		- Question 1;

--- main.cpp
+++ main.cpp
@@ -2,72 +2,10 @@
 
 int main()
 {
-    // Some language elements must be whitespace-separated
-
-    int x;
-    int                y;
-                int
-    z;
-
-    // Quoted text takes the amount of whitespace literally
-
-    std::cout << "Hello world!";
-    std::cout << "Hello          world!";
-
-    //std::cout << "Hello
-    // world!"; // Not allowed!
-
-    std::cout << "Hello "
-     "world!"; // prints "Hello world!"
-
-    // Basic formatting
-
-    std::cout << "This is a really, really, really, really, really, really, really, "
-        "really long line\n"; // one extra indentation for continuation line
-
-    std::cout << "This is another really, really, really, really, really, really, really, "
-                 "really long line\n"; // text aligned with the previous line for continuation line
-
-    std::cout << "This one is short\n";
-
-    std::cout << 3 + 4
-    + 5 + 6
-    * 7 * 8;
-    /* Harder to read
-        cost = 57;
-        pricePerItem = 24;
-        value = 5;
-        numberOfItems = 17;
-    */
-    //easy to read
-    int cost          = 57;
-    int pricePerItem  = 24;
-    int value         = 5;
-    int numberOfItems = 17;
-
-    std::cout << "Hello world!\n"; // cout lives in the iostream library
-    std::cout << "It is very nice to meet you!\n"; // these comments make the code hard to read
-    std::cout << "Yeah!\n"; // especially when lines are different lengths
-
-    std::cout << "Hello world!\n";                  // cout lives in the iostream library
-    std::cout << "It is very nice to meet you!\n";  // these comments are easier to read
-    std::cout << "Yeah!\n";                         // especially when all lined up
-
-    // cout lives in the iostream library
-    std::cout << "Hello world!\n";
-    // these comments make the code hard to read
-    std::cout << "It is very nice to meet you!\n";
-    // especially when all bunched together
-    std::cout << "Yeah!\n";
-
-    // cout lives in the iostream library
-    std::cout << "Hello world!\n";
-
-    // these comments are easier to read
-    std::cout << "It is very nice to meet you!\n";
-
-    // when separated by whitespace
-    std::cout << "Yeah!\n";
-
+    std::cout << "Enter an integer: ";
+    int num{};
+    std::cin >> num;
+    std::cout << "Double that number is: " << num * 2 << std::endl;
+    std::cout << "Triple that number is: " << num * 3 << std::endl;
     return 0;
 }
